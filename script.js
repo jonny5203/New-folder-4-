963689f91d0258232e217a9d67ec55debc2350ed
jonny5203 <jonnyanhvu@gmail.com>
2022-06-28 14:48:31 +0200

    liten feil

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function updateView() {
         }
 
         htmlCode += `</br></br><h1>Du har: ${points} poeng</h1>`;
-        htmlCode += `<h1>Highscore: ${points} poeng</h1>`;
+        htmlCode += `<h1>Highscore: ${highscore} poeng</h1>`;
 
     } else {
         htmlCode += /*HTML*/`
@@ -151,7 +151,7 @@ function gameOverFunction() {
         }
     };
 
-    if(points > highscore){
+    if(points >= highscore){
         highscore = points;
     }
 
